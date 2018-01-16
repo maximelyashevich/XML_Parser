d149b208f15e7de3842f40697a92b679ce1d9500
maximelyashevich <maximelyashevich@gmail.com>
2018-01-16 22:28:01 +0300

    Add file

--- src/com/elyashevich/task5/marshaller/PaperEditionMarshal.java
+++ src/com/elyashevich/task5/marshaller/PaperEditionMarshal.java
@@ -8,15 +8,11 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.util.Properties;
 import java.util.Set;
 
 public class PaperEditionMarshal {
@@ -45,36 +41,4 @@ public class PaperEditionMarshal {
             LOGGER.log(Level.ERROR, "JAXB-context is mistaken " + e);
         }
     }
-
-    public static void main(String[] args) {
-            Properties props = new Properties();
-            props.put("mail.smtp.host", "smtp.gmail.com");
-            props.put("mail.smtp.socketFactory.port", "465");
-            props.put("mail.smtp.socketFactory.class",
-                    "javax.net.ssl.SSLSocketFactory");
-            props.put("mail.smtp.auth", "true");
-            props.put("mail.smtp.port", "465");
-            Session session = Session.getDefaultInstance(props,
-                    new javax.mail.Authenticator() {
-                        @Override
-                        protected PasswordAuthentication getPasswordAuthentication() {
-                            return new PasswordAuthentication("maximelyashevich@gmail.com","gfhjkm[f");
-                        }
-                    });
-
-            try {
-
-                Message message = new MimeMessage(session);
-                message.setFrom(new InternetAddress("maximelyashevich@gmail.com"));
-                message.setRecipients(Message.RecipientType.TO,
-                        InternetAddress.parse("elyashevich1998@mail.ru"));
-                message.setSubject("Testing Subject");
-                message.setText("Test Mail");
-///comment
-                Transport.send(message);
-
-            } catch (MessagingException e) {
-                throw new RuntimeException(e);
-            }
-        }
 }
